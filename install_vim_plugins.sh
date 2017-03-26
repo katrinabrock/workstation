1c365409771df0cf6c0ebef3dcb02c214f523e0e
Katrina Brock <keb266@cornell.edu>
2017-03-26 09:24:45 -0700

    More vim plugins

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -1,6 +1,8 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle && \
+git clone https://github.com/tpope/vim-abolish
+git clone https://github.com/tpope/vim-fugitive
 git clone https://github.com/tpope/vim-sensible.git
 git clone https://github.com/tpope/vim-surround.git
 git clone https://github.com/tpope/vim-surround.git
@@ -10,3 +12,7 @@ git clone https://github.com/vim-syntastic/syntastic.git
 git clone https://github.com/epeli/slimux.git
 git clone https://github.com/python-rope/ropevim.git
 git clone https://github.com/fs111/pydoc.vim.git
+git clone https://github.com/leshill/vim-json
+git clone https://github.com/christoomey/vim-tmux-navigator
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/chrisbra/csv.vim
