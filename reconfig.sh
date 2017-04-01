033f5f391f1cb01dfac24264777af7438cd6abff
Katrina Brock <keb266@cornell.edu>
2017-04-01 11:47:13 -0700

    vim-like workspace switching

--- reconfig.sh
+++ reconfig.sh
@@ -1,2 +1,9 @@
 cp vimrc ~/.vimrc
 cp tmuxconf ~/.tmux.conf
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Control><Alt>l']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Control><Alt>h']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Control><Alt>k']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Control><Alt>j']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "<Ctrl><Alt>Escape"
+
