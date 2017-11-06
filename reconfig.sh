90fc0665ee09edf6d9d1a7280a997fa61083f503
Katrina Brock <katrina.brock@nokia.com>
2017-11-06 07:40:34 -0800

    Add todo

--- reconfig.sh
+++ reconfig.sh
@@ -1,4 +1,5 @@
 git clone --recursive https://github.com/katrinabrock/.vim ~/.vim
+# TODO: Use submodules for zsh config repos
 git clone https://github.com/sorin-ionescu/prezto.git ~/.zprezto
 git clone https://github.com/olivierverdier/zsh-git-prompt ~/zsh-git-prompt
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
