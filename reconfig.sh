46665be643caebf1a1fc8d1e7a072965d586073d
Katrina Brock <katrina.brock@nokia.com>
2017-11-06 07:37:42 -0800

    Add zsh helpers

--- reconfig.sh
+++ reconfig.sh
@@ -1,4 +1,7 @@
 git clone --recursive https://github.com/katrinabrock/.vim ~/.vim
+git clone https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+git clone https://github.com/olivierverdier/zsh-git-prompt ~/zsh-git-prompt
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
 ln ~/.vim/vimrc ~/.vimrc
 ln tmuxconf  ~/.tmux.conf
 ln .zshrc ~/.zshrc
