5bc4f10c09f48bdd21e8e3390423d03f771a968b
Katrina Brock <katrina.brock@nokia.com>
2017-11-11 13:23:11 -0800

    Make prezto installation work

--- reconfig.sh
+++ reconfig.sh
@@ -1,9 +1,19 @@
 git clone --recursive https://github.com/katrinabrock/.vim ~/.vim
 # TODO: Use submodules for zsh config repos
-git clone https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+
+# Install prezto
+git clone  --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+chsh -s /bin/zsh
+# But use custom zshrc
+rm ~/.zshrc
+ln zshrc ~/.zshrc
+
 git clone https://github.com/olivierverdier/zsh-git-prompt ~/zsh-git-prompt
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
 ln ~/.vim/vimrc ~/.vimrc
 ln tmuxconf  ~/.tmux.conf
-ln .zshrc ~/.zshrc
 
